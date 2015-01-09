eccb03d57a90432f8c1d5570eafdb8560a0cde60
hrj <harshad.rj@gmail.com>
2015-01-09 19:06:42 +0530

    updated to latest okhttp libraries
    copies directly downloaded from maven.org

--- src/Platform_Core/org/lobobrowser/security/LocalSecurityPolicy.java
+++ src/Platform_Core/org/lobobrowser/security/LocalSecurityPolicy.java
@@ -399,12 +399,12 @@ public class LocalSecurityPolicy extends Policy {
         permissions.add(new PropertyPermission("line.separator", "read"));
         permissions.add(new RuntimePermission("getClassLoader"));
 
-      } else if (path.endsWith("okhttp-urlconnection-2.1.1-SNAPSHOT.jar")) {
+      } else if (path.endsWith("okhttp-urlconnection-2.2.0.jar")) {
         permissions.add(new NetPermission("getProxySelector"));
         permissions.add(new NetPermission("getCookieHandler"));
         permissions.add(new PropertyPermission("http.*", "read"));
         permissions.add(new SocketPermission("*", "connect,resolve,listen,accept"));
-      } else if (path.endsWith("okhttp-2.1.1-SNAPSHOT.jar")) {
+      } else if (path.endsWith("okhttp-2.2.0.jar")) {
         permissions.add(new NetPermission("getProxySelector"));
         permissions.add(new NetPermission("getCookieHandler"));
         permissions.add(new PropertyPermission("http.*", "read"));
