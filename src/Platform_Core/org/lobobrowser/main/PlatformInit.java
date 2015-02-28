8e838f86b40194ad8b4a80fcf1623cd8ebe69e80
hrj <harshad.rj@gmail.com>
2015-02-28 09:46:41 +0530

    bumped release timeout period to 60 days

--- src/Platform_Core/org/lobobrowser/main/PlatformInit.java
+++ src/Platform_Core/org/lobobrowser/main/PlatformInit.java
@@ -76,7 +76,8 @@ import com.squareup.okhttp.OkUrlFactory;
  */
 public class PlatformInit {
   private static final String NATIVE_DIR_NAME = "native";
-  private static final long THIRTY_DAYS_MILLIS = 30 * 24 * 60 * 60 * 1000L;
+  private static final long DAYS_MILLIS = 24 * 60 * 60 * 1000L;
+  private static final long SIXTY_DAYS_MILLIS = 60 * DAYS_MILLIS;
   private final SimpleThreadPool threadExecutor;
 
   // private final GeneralSettings generalSettings;
@@ -316,15 +317,15 @@ public class PlatformInit {
       final String dateStr = relProps.getProperty(RELEASE_VERSION_RELEASE_DATE);
       final SimpleDateFormat yyyyMMDDFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
       final Date releaseDate = yyyyMMDDFormat.parse(dateStr);
-      final Date releaseDatePlus30Days = new Date(releaseDate.getTime() + THIRTY_DAYS_MILLIS);
+      final Date releaseDatePlus60Days = new Date(releaseDate.getTime() + SIXTY_DAYS_MILLIS);
       final Date currDate = new Date(System.currentTimeMillis());
-      if (releaseDatePlus30Days.before(currDate)) {
+      if (releaseDatePlus60Days.before(currDate)) {
         final String version = relProps.getProperty(RELEASE_VERSION_STRING);
         final String checkForUpdatesMessage = "<html><h3><center>This version of gngr is old</center></h3><p>gngr "
             + version
             + "</p><p>Released on: "
             + releaseDate
-            + "</p><p>This version is more than 30 days old and was not intended for long-time use.</p><p>Please check if a newer version is available on https://gngr.info</p></html>";
+            + "</p><p>This version is more than 60 days old and was not intended for long-time use.</p><p>Please check if a newer version is available on https://gngr.info</p></html>";
         JOptionPane.showMessageDialog(null, checkForUpdatesMessage);
       }
     } catch (IOException | ParseException e) {
