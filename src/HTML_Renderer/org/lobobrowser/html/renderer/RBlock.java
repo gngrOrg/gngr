586353d3d37b1533d15d37965a55ee5b0cf6ab2e
hrj <harshad.rj@gmail.com>
2015-08-22 18:20:57 +0530

    don't translate when painting selection for position:relative

--- src/HTML_Renderer/org/lobobrowser/html/renderer/RBlock.java
+++ src/HTML_Renderer/org/lobobrowser/html/renderer/RBlock.java
@@ -1109,7 +1109,6 @@ public class RBlock extends BaseElementRenderable {
   @Override
   public boolean paintSelection(final Graphics g, final boolean inSelection, final RenderableSpot startPoint, final RenderableSpot endPoint) {
     final Graphics newG = g.create();
-    newG.translate(relativeOffsetX, relativeOffsetY);
     try {
       final Insets insets = this.getInsetsMarginBorder(this.hasHScrollBar, this.hasVScrollBar);
       // Just clip, don't translate.
